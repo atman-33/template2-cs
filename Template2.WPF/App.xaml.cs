7643ef505b6ac77e322ab1d924693e7a44fc258b
Atman <41929192+Grawor@users.noreply.github.com>
2022-12-30 17:54:35 +0900

    プロジェクト ファイルを追加します。

---
+++ Template2.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using Template2.WPF.Views;
+
+namespace Template2.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
