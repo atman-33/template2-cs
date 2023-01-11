7f00c947c740727b7f6034385037b3b8a9189141
Atman <41929192+Grawor@users.noreply.github.com>
2023-01-11 23:07:31 +0900

    Sample002の画面を途中まで作成

---
+++ Template2.WPF/Views/Sample002View.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Template2.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for Sample002View
+    /// </summary>
+    public partial class Sample002View : UserControl
+    {
+        public Sample002View()
+        {
+            InitializeComponent();
+        }
+    }
+}
